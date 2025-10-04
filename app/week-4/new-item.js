4382ee190c402eb25fd6d54e380a6bedce1717bc
isayabs <isabelleyabut@gmail.com>
2025-10-03 18:49:50 -0600

    Initialize State Variables

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -2,3 +2,12 @@
 
 import { useState } from "react";
 
+export default function NewItem() {
+    const [quantity, setQuantity] = useState(1);
+
+    return (
+        <div>
+            <p>Quantity: {quantity}</p>
+        </div>
+    );
+}

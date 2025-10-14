e2b1a387c7c40c24f849dbabe94c10fddf2dfa12
isayabs <isabelleyabut@gmail.com>
2025-10-13 18:11:55 -0600

    Render items

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -45,15 +45,15 @@ export default function ItemList () {
             </div>
 
             <ul className="space-y-3">
-                {items.map((item) => (
+                {sorted.map((item) => (
                     <Item
-                    key={item.name}
-                    className="border rounded-lg p-4 shadow-sm max-w-xl mx-auto
+                        key={item.id}
+                        className="border rounded-lg p-4 shadow-sm max-w-xl mx-auto
                                 even:bg-[#93B7BE] even:text-gray-900
                                 odd:bg-[#2D3047] odd:text-white"
-                    name={item.name}
-                    quantity={item.quantity}
-                    category={item.category}
+                        name={item.name}
+                        quantity={item.quantity}
+                        category={item.category}
                     />
                 ))}
             </ul>

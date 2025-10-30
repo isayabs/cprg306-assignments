a681d8a1d16be20882060f35c6ff7412b553a10a
isayabs <isabelleyabut@gmail.com>
2025-10-29 23:53:17 -0600

    Modify item.js

--- app/week-8/item.js
+++ app/week-8/item.js
@@ -1,6 +1,13 @@
-export default function Item({ name, quantity, category, className = "" }) {
+export default function Item({ name, quantity, category, className = "", onSelect }) {
+  const handleClick = () => {
+    if (onSelect) onSelect({ name, quantity, category })
+  }
+
   return (
-    <li className={className}>
+    <li 
+      onClick={handleClick}
+      className={className}
+    >
       <p className="font-semibold">{name}</p>
       <p>Quantity: {quantity}</p>
       <p className="capitalize">Category: {category}</p>
